e9e6a45d8c1c05cd1aa0c41248102ab87ff0ace8
Arthur <41468119+ArthurGolubev@users.noreply.github.com>
2018-10-05 21:01:45 +0700

    Дзен питона. Проше - лучше. Добавь в каждый пакет переменную с выбором функций.

--- RasAsi_main.py
+++ RasAsi_main.py
@@ -7,21 +7,6 @@ RasAsiVer1.prost2()
 """
 необходим список всех доступных операций
 """
-capabilityPackeg = []
-for i in dir(RasAsiVer1):
-    if i.endswith('Packeg'):
-        capabilityPackeg.append(i)
-print(f'Список доступных пакетов (тест1)\n{capabilityPackeg}')
-
-print(len(capabilityPackeg))
-iterator1 = []
-for i in range(0, len(capabilityPackeg)):
-    iterator1.append(i+1)
-print(iterator1)
-dict1 = dict.fromkeys(capabilityPackeg, iterator1)
-print(dict1)
-
-
 
 
 """
