99b8abe02cdeadc8253f8a3d1b32c6d73141d63c
Arthur <41468119+ArthurGolubev@users.noreply.github.com>
2018-10-30 11:22:43 +0700

    Добавил распознование платформы и изменение путей сохранения под используемую платформу

--- RasAsiVer1/Download_Packeg/downloadScript_04.py
+++ RasAsiVer1/Download_Packeg/downloadScript_04.py
@@ -1,12 +1,17 @@
 import urllib.request, time, os, datetime
+from sys import platform
 
 
 def prost():
     print('privet from download!!')
 
 def download():
+    if platform == 'win32':
+        keypath1 = fr'D:\REMOTE SENSING IMG\to_download\purl_list'
+    elif platform == 'linux':
+        keypath1 = r'/media/pi/PORTABLE HDD/REMOTE SENSING IMG/to_download'
     file1_number = []
-    os.chdir(fr'D:\REMOTE SENSING IMG\to_download\purl_list')
+    os.chdir(fr'{keypath1}')
     listdir0 = os.listdir()
     print('Доступны файлы с ссылками:')
     variable1 = 0
@@ -14,13 +19,13 @@ def download():
         print(f'{variable1} - {i}')
         variable1 = variable1 + 1
     variable2 = int(input('\nУкажите порядковый номер файла\t'))
-    with open(rf'D:\REMOTE SENSING IMG\to_download\purl_list\{listdir0[variable2]}', 'r') as file1:
+    with open(os.path.normpath(rf'{keypath1}\{listdir0[variable2]}'), 'r') as file1:
         links_number = len(file1.readlines())                                                                           #пробегает по всем строчкам, возвращает количество
         file1.seek(0)                                                                                                   #Возвращает курсор в начало файла
         overallTime = time.time()                                                                                       #Присваевает начальное время запуска программы в секундах с начала эпохи переменной overallTime
         TimeNow = datetime.datetime.now()
         createlogName = f'{TimeNow.day}-{TimeNow.month}-{TimeNow.year}_time-{TimeNow.hour}-{TimeNow.minute}'
-        logFile = open(rf'D:\REMOTE SENSING IMG\to_download\logFile{createlogName}.txt', 'a')
+        logFile = open(os.path.normpath(rf'{keypath1}\logFile{createlogName}.txt'), 'a')
         logFile.write(f'Запуск программы\n{time.ctime()}\n')                                                            #записывает в файл текущую датувремя в понятной отформатированой форме
         logFile.close()
         for i in range(links_number - 1):                                                                               #считает i с 0, следовательно цифру количества ссылок (которая считается с 1, а не с 0) нужно убавить на 1
@@ -28,13 +33,13 @@ def download():
             string1 = file1.readline().strip()                                                                          # .strip() удаляет лишние элементы в строке, такие как не явно присутствующий символ переноса на следующую строку /n
             list1 = string1.split('/')                                                                                  #Делит строку URL-пути на список из названий
             try:                                                                                                        #Если такой папки нет - создаёт (нужно для первой ииерации при каждом новом названии директории)
-                os.makedirs(f'D:\REMOTE SENSING IMG\DigitalGlobe\{list1[3]}\{list1[4]}\{list1[5]}')
-                os.chdir(f'D:\REMOTE SENSING IMG\DigitalGlobe\{list1[3]}\{list1[4]}\{list1[5]}')
+                os.makedirs(os.path.normpath(f'{keypath1}\{list1[3]}\{list1[4]}\{list1[5]}'))
+                os.chdir(os.path.normpath(f'{keypath1}\{list1[3]}\{list1[4]}\{list1[5]}'))
 
-                downloadFuc(list1, string1, i, links_number, createlogName)
+                downloadFuc(list1, string1, i, links_number, createlogName, keypath1)
 
             except FileExistsError:                                                                                     #Если папка уже существует (создалоась при первой итерации для уникального названия директории)
-                os.chdir(f'D:\REMOTE SENSING IMG\DigitalGlobe\{list1[3]}\{list1[4]}\{list1[5]}')
+                os.chdir(os.path.normpath(f'{keypath1}\{list1[3]}\{list1[4]}\{list1[5]}'))
                 file1_number.extend(os.listdir())                                                                       #расширяемый список всех скачаных файлов (итерация 1 - файл1, файл2. итерация 2 - файл1, файл2, файл1, файл2, файл3)
                 print(f'файлов в папке - {len(set(file1_number))}\n')                                                   #длинна множества set уникальных имён файлов
                 v1 = len(set(file1_number))                                                                             #количество уникальных имён (скачанных файлов), а значит пройденных итераций по строкам файла с сылками
@@ -54,21 +59,21 @@ def download():
                     print(f'файл {list1[7]} пропущен')
                     continue                                                                                            #пропуск итераций до последнего скачанного файла
 
-                downloadFuc(list1, string1, i, links_number, createlogName)
+                downloadFuc(list1, string1, i, links_number, createlogName, keypath1)
 
-        with open(fr'D:\REMOTE SENSING IMG\to_download\logFile{createlogName}.txt', 'a') as logFile:                                #Общий лог за скачивание
+        with open(os.path.normpath(fr'{keypath1}\logFile{createlogName}.txt'), 'a') as logFile:                                #Общий лог за скачивание
             logFile.write('\n\n')
-            os.chdir(f'D:\REMOTE SENSING IMG\DigitalGlobe\{list1[3]}')
+            os.chdir(os.path.normpath(f'{keypath1}\{list1[3]}'))
             overallTime = time.time() - overallTime
             listdir1 = os.listdir()
             v2 = 0
             allSize = 0
             for i2 in listdir1:                                                                                         #Если скачивалось в 2 и более директории
                 sizeDir = 0
-                os.chdir(fr'D:\REMOTE SENSING IMG\DigitalGlobe\{list1[3]}\{i2}')
+                os.chdir(os.path.normpath(fr'{keypath1}\{list1[3]}\{i2}'))
                 listdir2 = os.listdir()
                 for i3 in listdir2:
-                    os.chdir(fr'D:\REMOTE SENSING IMG\DigitalGlobe\{list1[3]}\{i2}\{i3}')
+                    os.chdir(os.path.normpath(fr'{keypath1}\{list1[3]}\{i2}\{i3}'))
                     for i4 in os.listdir():
                         sizeDir = sizeDir + os.path.getsize(i4)
                 logFile.write(f'Директория {os.getcwd()}\nфайлов - {len(os.listdir())}\n')
@@ -82,13 +87,13 @@ def download():
     print('Done!')
     option1 = input('Задача завершина\nОткрыть файл-лог? (1/0)\t')
     if int(option1):
-        os.startfile(fr'D:\REMOTE SENSING IMG\to_download\logFile{createlogName}.txt')
+        os.startfile(os.path.normpath(fr'{keypath1}\logFile{createlogName}.txt'))
     else:
         return 0
 
 
-def downloadFuc(list1, string1, i, links_number, createlogName):
-    with open(fr'D:\REMOTE SENSING IMG\to_download\logFile{createlogName}.txt', 'a') as logFile:
+def downloadFuc(list1, string1, i, links_number, createlogName, keypath1):
+    with open(os.path.normpath(fr'{keypath1}\logFile{createlogName}.txt'), 'a') as logFile:
         print(f'Итерация №{i+1} из {links_number}\n')                                                                  #Позволяет перекачать последний файл в последовательности
         logFile.write(f'Итерация №{i+1} из {links_number}\n')                                                          #Нужно в случае, если последний запуск программы был прерван на середине загрузки файла
         startTime = time.time()
