60cdb38fa3a408765f60fb810286f7559fbecb24
Arthur <41468119+ArthurGolubev@users.noreply.github.com>
2018-11-05 22:10:19 +0700

    улучшил визуальную составляющую цикла перезаписи времени

--- RasAsiVer1/External_Packeg/electricity_monitoring.py
+++ RasAsiVer1/External_Packeg/electricity_monitoring.py
@@ -6,10 +6,11 @@ def func1():
     variable1 = 1
     print(time.ctime())
     while variable1:
-        print('func1_1')
+        print(r'*\|/ ____Записано____ \|/*')
+        print(f'Сейчас - {datetime.datetime.now()}')
         with open('/home/pi/Documents/logFileTime', 'w') as LF:
             LF.write(str(time.time()))
-        print('ok')
+        print(r'./|\ ____Записано____ /|\.')
         time.sleep(60)
 
 
@@ -26,20 +27,11 @@ def electricity_monitoringFunction():
             print(time.time())
             stopTime = datetime.timedelta(seconds=(time.time() - line1 - 180) // 1)
             print('try4')
-            print(stopTime)
+            print(f'Время простоя - {stopTime}')
             with open('/home/pi/Documents/StopTime', 'a') as LF:
                 LF.write(f'Дата - {datetime.datetime.now()} Время простоя - {str(stopTime)}\n')
-            # with open('/home/pi/Documents/StopTime', 'r') as LF:
-            #
-            #     def sf(LF):
-            #         list1= LF.readlines()
-            #         str1 = ''
-            #         for i in list1:
-            #             str1 = str1 + i + '<br/>'
-            #         print(str1)
-            #         return str1
-                time.sleep(10)
-                send(topic=f'Электричество - {time.ctime()}', message=log('/home/pi/Documents/StopTime'))
+            time.sleep(10)
+            send(topic=f'Электричество - {time.ctime()}', message=log('/home/pi/Documents/StopTime'))
 
 
     except:
