65bdbcf9ae1b9f27d305b2da3e6906818af69126
Arthur <41468119+ArthurGolubev@users.noreply.github.com>
2018-11-05 19:11:29 +0700

    отредактировал верное указание времени в datetime.datetime.now().year - указал атрибут .year

--- RasAsiVer1/External_Packeg/electricity_monitoring.py
+++ RasAsiVer1/External_Packeg/electricity_monitoring.py
@@ -27,8 +27,8 @@ def electricity_monitoringFunction():
             stopTime = datetime.timedelta(seconds=(time.time() - line1 - 180) // 1)
             print('try4')
             print(stopTime)
-            dateNow = f'{datetime.datetime.now().year}-{datetime.datetime.month}-{datetime.datetime.day}' \
-                      f' {datetime.datetime.hour}:{datetime.datetime.minute}:{datetime.datetime.second}'
+            dateNow = f'{datetime.datetime.now().year}-{datetime.datetime.now().month}-{datetime.datetime.now().day}' \
+                      f' {datetime.datetime.now().hour}:{datetime.datetime.now().minute}:{datetime.datetime.now().second}'
             print(f'mark #1\n\n\n\n{dateNow}')
             with open('/home/pi/Documents/StopTime', 'a') as LF:
                 LF.write(f'Дата - {dateNow} Время простоя - {str(stopTime)}\n')
