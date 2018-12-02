b6c459a4e9caa57e6e90104c289ab46d021292c9
Arthur <41468119+ArthurGolubev@users.noreply.github.com>
2018-12-02 22:46:57 +0700

    Пробую виджет с обнавлением лейбла

--- RasAsiVer1/External_Packeg/electricity_monitoring.py
+++ RasAsiVer1/External_Packeg/electricity_monitoring.py
@@ -1,5 +1,18 @@
 import time, datetime
 from RasAsiVer1.Gmail_Packeg.Send import send, log
+from PyQt5 import QtWidgets, QtGui
+from ..GUI.RasAsi_GUI import Ui_MainWindow
+import sys
+
+class mywindow(QtWidgets.QMainWindow):
+    def __init__(self):
+        super(mywindow, self).__init__()
+        self.ui = Ui_MainWindow()
+        self.ui.setupUi(self)
+
+    def f1(self, n):
+        self.ui.label.setFont(QtGui.QFont('SansSerif', 30))
+        self.ui.label.setText(f'{n}')
 
 
 def func1():
@@ -8,6 +21,7 @@ def func1():
     while variable1:
         print(r'*\|/_________Записано_________\|/*')
         print(f'    {datetime.datetime.now()}')
+        application.f1(datetime.datetime.now())
         with open('/home/pi/Documents/logFileTime', 'w') as LF:
             LF.write(str(time.time()))
         print(r'./|\_________Записано_________/|\.', '\n')
@@ -38,3 +52,8 @@ def electricity_monitoringFunction():
         func1()
 
     func1()
+app = QtWidgets.QApplication([])
+application = mywindow()
+application.show()
+
+sys.exit(app.exec())
