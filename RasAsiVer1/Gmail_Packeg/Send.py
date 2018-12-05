b629fd24568e5f0aadb254d8f2aa7e2125000dcc
Arthur <41468119+ArthurGolubev@users.noreply.github.com>
2018-12-05 23:30:12 +0700

    плохо работает на малинке

--- RasAsiVer1/Gmail_Packeg/Send.py
+++ RasAsiVer1/Gmail_Packeg/Send.py
@@ -1,7 +1,7 @@
 import httplib2
 import os
 import oauth2client
-from oauth2client import client, tools
+from oauth2client import client, tools, file
 import base64
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
