c83545b9ff57b8dc50ddc2a48f20c67588323acd
Arthur <41468119+ArthurGolubev@users.noreply.github.com>
2018-12-08 04:53:26 +0700

    Добавил остановку по указанию пользователя

--- RasAsiVer1/External_Packeg/electricity_monitoring.py
+++ RasAsiVer1/External_Packeg/electricity_monitoring.py
@@ -1,6 +1,7 @@
 import time, datetime
 from RasAsiVer1.Gmail_Packeg.Send import send, log
 
+
 def func1(t_stop):
     print(time.ctime())
     while not t_stop.is_set():
@@ -14,25 +15,17 @@ def func1(t_stop):
 
 def electricity_monitoringFunction(t_stop):
     try:
-        print('try1')
         with open('/home/pi/Documents/logFileTime', 'r') as LF:
-            print('try2')
             line1 = float(LF.readline())
-            print(line1)
-            # line2 = LF.readline()
-            # print(f'MARK #10000 {line2}')
-            print('try3')
-            print(time.time())
-            time.sleep(5)
-            print(time.time())
+            time.sleep(180)
             stopTime = datetime.timedelta(seconds=int(time.time() - line1 - 180))
-            print('try4')
-            print(f'Время простоя - {stopTime}')
+            print(f'\nВремя простоя - {stopTime}')
             with open('/home/pi/Documents/StopTime', 'a') as LF1:
                 if LF.readline():
                     LF1.write(f'Дата - {datetime.datetime.now()} Время простоя - {str(stopTime)} *user stop*\n')
                 else:
                     LF1.write(f'Дата - {datetime.datetime.now()} Время простоя - {str(stopTime)}\n')
+
         send(topic=f'Электричество - {time.ctime()}', message=log('/home/pi/Documents/StopTime'))
 
     except:
