92922c7a28ac9c7fdc0854b07943084180a5f0bc
Arthur <41468119+ArthurGolubev@users.noreply.github.com>
2019-02-03 01:44:02 +0700

    Тест selenium chrome on linux

--- RasAsiVer1/RMafterTEST/selenium1.py
+++ RasAsiVer1/RMafterTEST/selenium1.py
@@ -1,5 +1,5 @@
 from selenium import webdriver
 
-browser = webdriver.Firefox()
+driver = webdriver.Firefox(firefox_profile=profile, log_path='home/pi/RasAsi/geckodriver.log')
 
 browser.get('https://24-ok.ru/')
