4b74b739d9f0aaec78d8e8340fa654700293234d
Arthur <41468119+ArthurGolubev@users.noreply.github.com>
2019-03-17 10:21:11 +0700

    123

--- RasAsiVer2/testSeleniumOnRasAsi.py
+++ RasAsiVer2/testSeleniumOnRasAsi.py
@@ -1,4 +1,4 @@
 from selenium import webdriver
 
-browser = webdriver.Firefox(executable_path='/home/pi/RasAsi/RasAsiVer2/geckodriver')
+browser = webdriver.Firefox(executable_path='/home/pi/RasAsi/RasAsiVer2/geckodriver', capabilities={"marionette": False})
 browser.get('https://www.google.ru/')
