7c698714aaf5932fe9550d0d31592a37b620e3e1
Arthur <41468119+ArthurGolubev@users.noreply.github.com>
2019-03-17 11:26:13 +0700

    123

--- RasAsiVer2/testSeleniumOnRasAsi.py
+++ RasAsiVer2/testSeleniumOnRasAsi.py
@@ -2,5 +2,8 @@ import sys
 sys.path.append('/usr/local/lib/python3.7/site-packages')
 from selenium import webdriver
 
+from pyvirtualdisplay import Display
+display = Display(visible=0, size=(800, 800))
+display.start()
 browser = webdriver.Chrome(executable_path="/usr/lib/chromium-browser/chromedriver")
 browser.get('https://www.google.com/')
