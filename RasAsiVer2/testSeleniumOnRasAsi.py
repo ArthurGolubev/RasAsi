e8a53c82f76f1d52f50d31a331143e13694d44d3
Arthur <41468119+ArthurGolubev@users.noreply.github.com>
2019-03-17 11:10:22 +0700

    123

--- RasAsiVer2/testSeleniumOnRasAsi.py
+++ RasAsiVer2/testSeleniumOnRasAsi.py
@@ -4,7 +4,7 @@ options = webdriver.ChromeOptions()
 options.add_argument("-headless")
 options.add_argument("-disable-gpu")
 
-chrome = webdriver.Chrome(chrome_options=options, executable_path="/usr/lib/chromium-browser/chromedriver")
+chrome = webdriver.Chrome(executable_path="/usr/lib/chromium-browser/chromedriver")
 chrome.get("http://google.fr")
 
 print("Titre de la page: {}".format(chrome.title))
