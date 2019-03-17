91b77a3a39e64d168e6dcc1ad96eab6813af3f0a
Arthur <41468119+ArthurGolubev@users.noreply.github.com>
2019-03-17 10:53:38 +0700

    123

--- RasAsiVer2/testSeleniumOnRasAsi.py
+++ RasAsiVer2/testSeleniumOnRasAsi.py
@@ -1,4 +1,11 @@
 from selenium import webdriver
 
-browser = webdriver.Chrome()
-browser.get('https://www.google.ru/')
+options = webdriver.ChromeOptions()
+options.add_argument("-headless")
+options.add_argument("-disable-gpu")
+
+chrome = webdriver.Chrome(chrome_options=options)
+chrome.get("http://google.fr")
+
+print("Titre de la page: {}".format(chrome.title))
+chrome.save_screenshot('screenshot.png')
