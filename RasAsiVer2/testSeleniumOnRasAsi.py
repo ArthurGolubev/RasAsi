4a7c7b2c832bfad72414d75b0d064e3cf1da35d2
Arthur <41468119+ArthurGolubev@users.noreply.github.com>
2019-03-17 12:29:53 +0700

    123

--- RasAsiVer2/testSeleniumOnRasAsi.py
+++ RasAsiVer2/testSeleniumOnRasAsi.py
@@ -5,5 +5,5 @@ from selenium import webdriver
 from pyvirtualdisplay import Display
 display = Display(visible=0, size=(800, 800))
 display.start()
-browser = webdriver.Firefox()
+browser = webdriver.Firefox(executable_path='/home/pi/RasAsi/RasAsiVer2/geckodriver')
 browser.get('https://www.google.com/')
