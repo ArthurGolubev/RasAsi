394620f6fdd5470886db0ddb192b094e5e0ae0e6
Arthur <41468119+ArthurGolubev@users.noreply.github.com>
2019-03-27 18:36:24 +0700

    TodayTasks

--- RasAsiVer1/Time_Packeg/time_management.py
+++ RasAsiVer1/Time_Packeg/time_management.py
@@ -24,7 +24,7 @@ def k2(t_stop):
         if cTime.hour == 00 and cTime.minute == 00:
             msg = emojize(f'{ej["слон"]} Время работы сервера:\t {str(timeHasPassed(startTimeRasAsi))}')
             send(topic='Server time ☁', message=msg)
-        elif cTime.hour == 18 and cTime.minute == 35:
+        elif cTime.hour == 18 and cTime.minute == 40:
             print('\n\n\n\nok\n')
             Tasks.take_tasks()
         try:
