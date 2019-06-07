d9f4f267b41ecc9701acc704bdcc7c5065a64b62
Arthur <41468119+ArthurGolubev@users.noreply.github.com>
2019-06-08 00:10:49 +0700

    one credential init

--- RasAsiVer2/Google/GoogleTasks.py
+++ RasAsiVer2/Google/GoogleTasks.py
@@ -14,7 +14,7 @@ class GoogleTasks:
         _client_secret = path_credential + r'\client_secret.json'
     elif platform == 'linux':
         # path_credential = r'/home/pi/Downloads'  # raspbian
-        path_credential = r'~/PycharmProjects/RasAsi/credential'  # Ubuntu
+        path_credential = r'/PycharmProjects/RasAsi/credential'  # Ubuntu
         _client_secret = path_credential + r'/client_secret.json'
     else:
         print(f'Платформа {platform} не поддерживается')
