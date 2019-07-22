016ba34e83517ccd3cc0847559af89860cb8f5fb
Arthur <41468119+ArthurGolubev@users.noreply.github.com>
2019-07-23 00:27:15 +0700

    migration GoogleSpreadsheet to PostgreSQL

--- RasAsiVer2/Database_Scripts/dump_database.py
+++ RasAsiVer2/Database_Scripts/dump_database.py
@@ -6,7 +6,7 @@ def dump_rasasi_database():
     cTime = datetime.datetime.now().date()-datetime.timedelta(days=1)
     print(cTime)
     path = fr'/home/rasasi/dump_database_rasasi/{cTime}.sql'
-    subprocess.Popen(fr'pg_dump rasasi_database > {path}')
+    subprocess.Popen(fr'pg_dump rasasi_database > {path}', shell=True, executable='/bin/bash')
     GoogleDrive().upload(files=path, folder_id='1CpsaUbjn2_4Zm6Sog05BBQwf3MEqQ2Vk')
     print('DATABASE DUMP WAS SUCCESS')
 
