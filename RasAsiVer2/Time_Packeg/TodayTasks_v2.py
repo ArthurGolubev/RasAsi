13d3ea115e561b2c05322ca9ed635f1bc9d47dae
Arthur <41468119+ArthurGolubev@users.noreply.github.com>
2019-07-27 20:05:39 +0700

    migration GoogleSpreadsheet to PostgreSQL

--- RasAsiVer2/Time_Packeg/TodayTasks_v2.py
+++ RasAsiVer2/Time_Packeg/TodayTasks_v2.py
@@ -62,7 +62,6 @@ class TodayTasksV2:
                                       ).list_tasks(completedMin=today)
 
         for i in completed_tasks:
-            comment = ''
             id_task = None
 
             for i2 in self._snapshot_my_storage:
