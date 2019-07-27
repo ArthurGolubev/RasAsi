0f055d4746bafd92fe0c5e46ec4a5e9331fcaa19
Arthur <41468119+ArthurGolubev@users.noreply.github.com>
2019-07-27 21:34:51 +0700

    migration GoogleSpreadsheet to PostgreSQL

--- RasAsiVer2/Time_Packeg/TodayTasks_v2.py
+++ RasAsiVer2/Time_Packeg/TodayTasks_v2.py
@@ -47,8 +47,8 @@ class TodayTasksV2:
         :return: Nothing
         """
         for i in self._snapshot_my_storage:
-            print('i[0] - ', type(i[0]))
-            print(i[0], ' - ', num)
+            # print('i[0] - ', type(i[0]))
+            # print(i[0], ' - ', num)
             if i[0] == num:
                 input('pause\t')
                 GoogleTasks(mainID=self._tasklist_id).insert(task={'title': i[1]})
@@ -59,7 +59,9 @@ class TodayTasksV2:
         Shows completed today (from 00:00 today). Checks for tags. Marks the event in the corresponding tag table
         :return:
         """
+        print('len before - ', len(self._snapshot_my_storage))
         self.snapshot_my_storage()
+        print('len after - ', len(self._snapshot_my_storage))
         cTime = datetime.now().time()
         today = (datetime.utcnow() - timedelta(hours=cTime.hour, minutes=cTime.minute, seconds=cTime.second)
                  ).isoformat('T') + 'Z'
